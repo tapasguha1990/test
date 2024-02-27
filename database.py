4d64dcb36405c6922650c3e9bfe77386c4904941
tapasguha1990 <147483461+tapasguha1990@users.noreply.github.com>
2024-02-27 10:03:15 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class NSE_SESSION:
     def __init__(self):
-        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        self.headers = {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
             'accept-language': 'en,gu;q=0.9,hi;q=0.8',
             'accept-encoding': 'gzip, deflate, br'}
         self.cook_url = "https://www.nseindia.com/option-chain"
