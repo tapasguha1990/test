bd8d0614a28b9a2d3c7b5d4340653feba3deeb82
tapasguha1990 <147483461+tapasguha1990@users.noreply.github.com>
2024-02-27 10:26:29 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -5,22 +5,19 @@ from datetime import datetime
 
 class NSE_SESSION:
     def __init__(self):
-        self.headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
-                          'like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
             'accept-language': 'en,gu;q=0.9,hi;q=0.8',
             'accept-encoding': 'gzip, deflate, br'}
         self.cook_url = "https://www.nseindia.com/option-chain"
-
+        self.session = requests.Session()
+        self.cookies = self.session.get(self.cook_url, headers=self.headers , timeout = 5).cookies
 
     def GetExpiry(self,indices):
         url = f'https://www.nseindia.com/api/option-chain-indices?symbol={indices}'
         input_format = "%d-%b-%Y"
         output_format = "%d%b%y"
         try:
-            self.session = requests.Session()
-            cookies = self.session.get(self.cook_url, headers=self.headers , timeout = 5).cookies
-            response = self.session.get(url,headers=self.headers, timeout=5, cookies=cookies)
+            response = self.session.get(url,headers=self.headers, timeout=5, cookies=self.cookies)
             if response.status_code == 200:
                 records = response.json()['records']
                 format_exp = [datetime.strptime(date, input_format).strftime(output_format).upper() for date in
--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def index():
 
     return render_template('index.html', expiry_dates=expiry_dates)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
